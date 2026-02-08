032e72079dfaab44eb46868d222415f099cb4492
Your Name <153941695+Maks-Mm@users.noreply.github.com>
2026-02-08 22:36:12 +0100

    new routing for dashboard native

---
+++ apps/native/app/(protected)/dashboard/index.tsx
@@ -0,0 +1,11 @@
+//app/native/app/(protected)/dashboard/index.tsx
+
+import { View, Text } from 'react-native';
+
+export default function DashboardScreen() {
+  return (
+    <View>
+      <Text>Dashboard Native 1</Text>
+    </View>
+  );
+}
